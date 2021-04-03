4c25dc537be34c50f3e76c73c8ea4761c4c04e73
ParkJiwoon <bcp0109@gmail.com>
2021-04-03 16:20:45 +0900

    first commit

---
+++ jackjson-and-lombok/src/test/java/com/practice/jacksonandlombok/JacksonAndLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jacksonandlombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonAndLombokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
