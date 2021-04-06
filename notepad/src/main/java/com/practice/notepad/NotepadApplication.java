717d033c51be571e355baa7f3525cbe3c14e7821
ParkJiwoon <bcp0109@gmail.com>
2021-04-06 23:17:09 +0900

    낙서장 추가

---
+++ notepad/src/main/java/com/practice/notepad/NotepadApplication.java
@@ -0,0 +1,13 @@
+package com.practice.notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotepadApplication.class, args);
+    }
+
+}
---
+++ notepad/src/test/java/com/practice/notepad/NotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
