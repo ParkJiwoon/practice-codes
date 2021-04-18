54ea5d6c3712252ee15c154a7712ac216c51831b
ParkJiwoon <bcp0109@gmail.com>
2021-04-19 03:02:45 +0900

    build: H2 Database 연결

--- spring-jpa-querydsl/src/test/java/com/practice/springjpaquerydsl/entity/HelloTest.java
+++ spring-jpa-querydsl/src/test/java/com/practice/springjpaquerydsl/entity/HelloTest.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit // 테스트가 끝나도 DB 데이터를 바로 지우지 않음
 public class HelloTest {
 
     @Autowired
