512ee109fb080473171649e35aef6d6960499b08
ParkJiwoon <bcp0109@gmail.com>
2021-04-23 23:58:44 +0900

    add spring test project

---
+++ spring-test/src/main/java/com/practice/springtest/SpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+
+}
