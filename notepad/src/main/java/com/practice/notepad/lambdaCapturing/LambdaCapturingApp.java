4788c886905d75b72230aa4a1490754e2e0d59d0
ParkJiwoon <bcp0109@gmail.com>
2021-05-12 00:59:38 +0900

    lambda capturing 예시

---
+++ notepad/src/main/java/com/practice/notepad/lambdaCapturing/LambdaCapturingApp.java
@@ -0,0 +1,8 @@
+package com.practice.notepad.lambdaCapturing;
+
+public class LambdaCapturingApp {
+    public static void main(String[] args) {
+        Lambda lambda = new Lambda();
+        lambda.localMethod();
+    }
+}
