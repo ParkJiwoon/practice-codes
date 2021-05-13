2cd5acbde889fe7094cd67b797d02c4014985bf2
ParkJiwoon <bcp0109@gmail.com>
2021-05-14 01:59:59 +0900

    initialize spring transactional project

---
+++ spring-transactional/src/main/java/com/practice/transactional/TransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.practice.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalApplication.class, args);
+    }
+
+}
