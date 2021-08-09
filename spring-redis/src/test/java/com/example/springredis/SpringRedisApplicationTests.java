4f7c51fa4d06c7d3003a68056562f14713ae2953
ParkJiwoon <bcp0109@gmail.com>
2021-08-09 20:10:30 +0900

    spring-redis: first commit

---
+++ spring-redis/src/test/java/com/example/springredis/SpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
