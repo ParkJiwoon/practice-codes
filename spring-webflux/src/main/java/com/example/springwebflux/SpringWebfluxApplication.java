338f9c5264fe1d634bda875cbce6e2055b2e14f2
ParkJiwoon <bcp0109@gmail.com>
2021-08-14 21:00:27 +0900

    spring-webflux: first commit

---
+++ spring-webflux/src/main/java/com/example/springwebflux/SpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxApplication.class, args);
+    }
+
+}
