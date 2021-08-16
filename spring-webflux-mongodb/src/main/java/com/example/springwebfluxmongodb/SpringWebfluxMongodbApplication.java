50f644fe90d46233b1b6de0a22d4facf8ea592a3
ParkJiwoon <bcp0109@gmail.com>
2021-08-16 23:00:42 +0900

    spring-webflux-mongodb: first commit

---
+++ spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/SpringWebfluxMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxMongodbApplication.class, args);
+    }
+
+}
