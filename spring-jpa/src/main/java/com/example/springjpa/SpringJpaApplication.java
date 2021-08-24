dd89ce9b8d8e6b201e213d3b8cf5d1f1b530cef5
ParkJiwoon <bcp0109@gmail.com>
2021-08-24 17:07:47 +0900

    spring-jpa: first commit

---
+++ spring-jpa/src/main/java/com/example/springjpa/SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaApplication.class, args);
+    }
+
+}
---
+++ spring-jpa/src/test/java/com/example/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
