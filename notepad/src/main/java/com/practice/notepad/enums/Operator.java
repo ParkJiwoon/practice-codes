4ac8db86c164df137d9b599cfc66a48729ec1e98
ParkJiwoon <bcp0109@gmail.com>
2021-08-24 17:06:30 +0900

    notdpad: enum 클래스 테스트

--- notepad/src/main/java/com/practice/notepad/enums/Operator.java
+++ notepad/src/main/java/com/practice/notepad/enums/Operator.java
@@ -1,21 +1,49 @@
 package com.practice.notepad.enums;
 
+import java.util.Arrays;
+
 public enum Operator {
-    PLUS(new PlusCalculator()),
-    MINUS(new MinusCalculator()),
+    PLUS(new PlusCalculator(), "더하기"),
+    MINUS(new MinusCalculator(), "빼기"),
     ;
 
     private final Calculator calculator;
+    private final String krName;
 
-    Operator(Calculator calculator) {
+    Operator(Calculator calculator, String krName) {
         this.calculator = calculator;
+        this.krName = krName;
     }
 
-    int calculate(int a, int b) {
+    public String getKrName() {
+        return krName;
+    }
+
+    public int calculate(int a, int b) {
         return calculator.calculate(a, b);
     }
 
-    void printCalculator() {
+    public void printCalculator() {
         System.out.println(calculator);
+    }
+
+    public String concat(String str) {
+        return name().concat(str);
+    }
+
+    public static String translateEnToKr1(String enName) {
+        try {
+            return valueOf(enName).krName;
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    public static String translateEnToKr2(String enName) {
+        return Arrays.stream(values())
+                .filter(operator -> operator.name().equals(enName))
+                .findAny()
+                .map(operator -> operator.krName)
+                .orElse(null);
     }
 }
