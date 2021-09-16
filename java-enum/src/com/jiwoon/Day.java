955917ec192cfa2739726573b015ea733d58b8b1
ParkJiwoon <bcp0109@gmail.com>
2021-09-16 09:48:40 +0900

    java-enum: Enum 예제 코드 작성

---
+++ java-enum/src/com/jiwoon/Day.java
@@ -0,0 +1,48 @@
+package com.jiwoon;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Day {
+    MON("Monday", 10),
+    TUE("Tuesday", 20),
+    WED("Wednesday", 30),
+    THU("Thursday", 40),
+    FRI("Friday", 50),
+    SAT("Saturday", 60),
+    SUN("Sunday", 70)
+    ;
+
+    private final String label;
+    private final int number;
+
+    Day(String label, int number) {
+        this.label = label;
+        this.number = number;
+    }
+
+    public String label() {
+        return label;
+    }
+
+    public int number() {
+        return number;
+    }
+
+    private static final Map<String, Day> BY_LABEL =
+            Stream.of(values()).collect(Collectors.toMap(Day::label, Function.identity()));
+
+    private static final Map<Integer, Day> BY_NUMBER =
+            Stream.of(values()).collect(Collectors.toMap(Day::number, Function.identity()));
+
+    public static Day valueOfLabel(String label) {
+        return BY_LABEL.get(label);
+    }
+
+    public static Day valueOfNumber(int number) {
+        return BY_NUMBER.get(number);
+    }
+}
--- java-enum/src/com/jiwoon/Main.java
+++ java-enum/src/com/jiwoon/Main.java
@@ -3,6 +3,12 @@ package com.jiwoon;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(Day.MON.name());      // MON
+        System.out.println(Day.MON.label());       // Monday
+        System.out.println(Day.MON.ordinal());   // 0
+
+
+        System.out.println(Day.valueOfLabel("Sunday"));
+        System.out.println(Day.valueOfNumber(30));
     }
 }
