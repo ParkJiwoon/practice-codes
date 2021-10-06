ccfa6a2fc98e8b69cf0537fc7b1f9a34646e7c94
ParkJiwoon <bcp0109@gmail.com>
2021-10-07 02:20:06 +0900

    spring-webflux-mongodb: 일반적인 CureRepository 의 메소드와 커스텀 쿼리 적용한 메소드

--- spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/repository/ItemRepository.java
+++ spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/repository/ItemRepository.java
@@ -1,7 +1,16 @@
 package com.example.springwebfluxmongodb.repository;
 
 import com.example.springwebfluxmongodb.entity.Item;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
+    Flux<Item> findByNameContaining(String partialName);
+
+    @Query("{ 'name' : ?0, 'age' : ?1 }")
+    Flux<Item> findItemsForCustomerMonthlyReport(String name, int age);
+
+    @Query(sort = "{ 'age' : -1 }")
+    Flux<Item> findSortedStuffForWeeklyReport();
 }
