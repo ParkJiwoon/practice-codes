ee8e6e48929508e15d0869f618549d65278cf1fd
ParkJiwoon <bcp0109@gmail.com>
2021-10-06 21:01:56 +0900

    spring-webflux-mongodb: 비동기 논블로킹으로 사전 데이터 세팅하기 위한 의존성 추가

--- spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/loader/TemplateDatabaseLoader.java
+++ spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/loader/TemplateDatabaseLoader.java
@@ -3,14 +3,17 @@ package com.example.springwebfluxmongodb.loader;
 import com.example.springwebfluxmongodb.entity.Item;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 
+/**
+ * Blocking 레포지토리 대신 MongoOperations 을 사용하여 비동기, 논블로킹 방식으로 데이터 세팅
+ */
 @Component
 public class TemplateDatabaseLoader {
 
     @Bean
-    public CommandLineRunner initialize(ReactiveMongoOperations mongo) {
+    public CommandLineRunner initialize(MongoOperations mongo) {
         return args -> {
             mongo.save(new Item("Alf alarm clock", 19.99));
             mongo.save(new Item("Smurf TV tray", 24.99));
