cd8579efd681c221229701fcef687b5511f543b0
ParkJiwoon <bcp0109@gmail.com>
2021-10-11 19:15:15 +0900

    spring-webflux-mongodb: Controller 계층 API 응답 테스트
    - 개별 테스트는 성공하는데 전체 테스트는 실패하는 이슈가 있어서 테스트용 application properties 생성

---
+++ spring-webflux-mongodb/src/test/java/com/example/springwebfluxmongodb/controller/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.springwebfluxmongodb.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+// @SpringBootTest : @SpringBootApplication 이 붙은 클래스를 찾아서 내장 컨테이너를 실행함
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class HomeControllerTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @DisplayName("Home 페이지에는 add API 가 존재")
+    @Test
+    void homePageContainAddApi() {
+        client.get().uri("/").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.TEXT_HTML)
+                .expectBody(String.class)
+                .consumeWith(exchangeResult -> {
+                    assertThat(exchangeResult.getResponseBody()).contains("<form method=\"post\" action=\"/add");
+                });
+    }
+}
