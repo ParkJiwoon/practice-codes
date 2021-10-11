814a42f08d45f4116ab6c248e4a6bb3499442107
ParkJiwoon <bcp0109@gmail.com>
2021-10-11 17:28:03 +0900

    spring-webflux-mongodb: 프로젝트 내의 Blocking 코드를 검출하는 BlockHound 추가
    - 프로젝트 내에 있는 코드 뿐만 아니라 JDK 의 블로킹 코드까지 전부 검출한다
    - 타임리프 템플릿 엔진에서 호출하는 FileInputStream.readBytes() 가 블로킹 코드라서 에러가 발생하니 그부분만 허용

--- spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/SpringWebfluxMongodbApplication.java
+++ spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/SpringWebfluxMongodbApplication.java
@@ -2,11 +2,17 @@ package com.example.springwebfluxmongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringWebfluxMongodbApplication {
 
     public static void main(String[] args) {
+        BlockHound.builder()
+                .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
+                .install();
+
         SpringApplication.run(SpringWebfluxMongodbApplication.class, args);
     }
 
