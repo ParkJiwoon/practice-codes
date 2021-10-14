c5e9c2e58afcc1f5ce9fa3cbe6083efc0f87d7e2
ParkJiwoon <bcp0109@gmail.com>
2021-10-15 03:23:36 +0900

    spring-webflux-mongodb: BlockHound 때문에 JAR 실행 실패해서 주석 처리

--- spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/SpringWebfluxMongodbApplication.java
+++ spring-webflux-mongodb/src/main/java/com/example/springwebfluxmongodb/SpringWebfluxMongodbApplication.java
@@ -2,16 +2,14 @@ package com.example.springwebfluxmongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thymeleaf.TemplateEngine;
-import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringWebfluxMongodbApplication {
 
     public static void main(String[] args) {
-        BlockHound.builder()
-                .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
-                .install();
+//        BlockHound.builder()
+//                .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
+//                .install();
 
         SpringApplication.run(SpringWebfluxMongodbApplication.class, args);
     }
