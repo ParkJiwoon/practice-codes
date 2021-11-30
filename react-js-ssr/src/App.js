567e1410b61b024a599f1ebbdfc1431a2feb0874
ParkJiwoon <bcp0109@gmail.com>
2021-11-30 20:31:59 +0900

    react-js-ssr: yarn eject 전에 커밋

--- react-js-ssr/src/App.js
+++ react-js-ssr/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import RedPage from './pages/RedPage';
+import BluePage from './pages/BluePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
     </div>
   );
 }
---
+++ react-js-ssr/src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/red">Red</Link>
+      </li>
+      <li>
+        <Link to="/blue">Blue</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Menu;
