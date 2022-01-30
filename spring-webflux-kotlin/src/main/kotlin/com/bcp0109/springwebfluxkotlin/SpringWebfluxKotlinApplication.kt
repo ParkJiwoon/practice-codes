2e34bec6a64810a5b52aeb9bbc1603f7e6a4946b
ParkJiwoon <bcp0109@gmail.com>
2022-01-31 03:52:13 +0900

    spring-webflux-kotlin: first commit

---
+++ spring-webflux-kotlin/src/main/kotlin/com/bcp0109/springwebfluxkotlin/SpringWebfluxKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.bcp0109.springwebfluxkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebfluxKotlinApplication>(*args)
+}
---
+++ spring-webflux-kotlin/src/test/kotlin/com/bcp0109/springwebfluxkotlin/SpringWebfluxKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bcp0109.springwebfluxkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
