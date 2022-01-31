5343456afc8080bb9a37ea5a5d0e524d5747bbe5
ParkJiwoon <bcp0109@gmail.com>
2022-02-01 02:56:03 +0900

    design_pattern: lombok 제거

--- design_pattern/build.gradle.kts
+++ design_pattern/build.gradle.kts
@@ -26,8 +26,6 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
