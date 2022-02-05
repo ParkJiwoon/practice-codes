f15a0257e1b40ace86cebfe07f8664277a492b48
ParkJiwoon <bcp0109@gmail.com>
2022-02-05 22:00:18 +0900

    spring_boot_aop: aop 디펜던시 추가

--- spring_aop/spring_boot_aop/build.gradle.kts
+++ spring_aop/spring_boot_aop/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
