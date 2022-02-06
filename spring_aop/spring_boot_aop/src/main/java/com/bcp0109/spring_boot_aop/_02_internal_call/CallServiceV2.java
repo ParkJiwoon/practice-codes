7a399f7004a662c586ff66b30d618056f921d527
ParkJiwoon <bcp0109@gmail.com>
2022-02-06 18:02:37 +0900

    spring_aop: 내부 메서드 호출 해결법 1 - 지연 조회

---
+++ spring_aop/spring_boot_aop/src/main/java/com/bcp0109/spring_boot_aop/_02_internal_call/CallServiceV2.java
@@ -0,0 +1,34 @@
+package com.bcp0109.spring_boot_aop._02_internal_call;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CallServiceV2 {
+
+    /**
+     * 프록시와 내부호출 해결법 - 지연 조회
+     *
+     * 셀프 주입과 비슷하지만 메서드 호출 시점에 빈을 꺼낸다는게 차이점
+     * 순환 참조가 발생하지 않음
+     */
+
+//    private final ApplicationContext applicationContext;
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public CallServiceV2(ObjectProvider<CallServiceV2> callServiceProvider) {
+        this.callServiceProvider = callServiceProvider;
+    }
+
+    public void external() {
+        System.out.println("call external");
+//        CallServiceV2 callServiceV2 = applicationContext.getBean(CallServiceV2.class);
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal(); // 외부 메서드 호출
+    }
+
+    public void internal() {
+        System.out.println("call internal");
+    }
+}
---
+++ spring_aop/spring_boot_aop/src/test/java/com/bcp0109/spring_boot_aop/_02_internal_call/CallServiceV2Test.java
@@ -0,0 +1,28 @@
+package com.bcp0109.spring_boot_aop._02_internal_call;
+
+import com.bcp0109.spring_boot_aop._02_internal_call.aop.CallLogAspect;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV2Test {
+
+    @Autowired
+    CallServiceV2 callServiceV2;
+
+    @Test
+    @DisplayName("Bean 을 호출 시점에 꺼내서 호출함. 인스턴스로 호출하기 때문에 AOP 적용되어 있음")
+    void external() {
+        callServiceV2.external();
+    }
+
+    @Test
+    @DisplayName("외부에서 호출하는 internal 에는 AOP 가 적용됨")
+    void internal() {
+        callServiceV2.internal();
+    }
+}
