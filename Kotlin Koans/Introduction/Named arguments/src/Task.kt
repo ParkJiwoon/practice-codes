0f255eb980fbb9f3e33504a507ac6124704afccc
ParkJiwoon <bcp0109@gmail.com>
2022-02-17 23:38:50 +0900

    Kotlin Koans: Named arguments

--- Kotlin Koans/Introduction/Named arguments/src/Task.kt
+++ Kotlin Koans/Introduction/Named arguments/src/Task.kt
@@ -1,2 +1,5 @@
 fun joinOptions(options: Collection<String>) =
-        options.joinToString(TODO())
+        options.joinToString(
+                prefix = "[",
+                postfix = "]"
+        )
