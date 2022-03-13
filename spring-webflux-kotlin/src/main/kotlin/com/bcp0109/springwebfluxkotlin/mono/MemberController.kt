57ae99fc1acc1dc43492ed5680bf2e66e9e535e8
ParkJiwoon <bcp0109@gmail.com>
2022-03-13 21:37:13 +0900

    spring-webflux-kotlin: 외부 요청하는 API 추가

--- spring-webflux-kotlin/src/main/kotlin/com/bcp0109/springwebfluxkotlin/mono/MemberController.kt
+++ spring-webflux-kotlin/src/main/kotlin/com/bcp0109/springwebfluxkotlin/mono/MemberController.kt
@@ -1,10 +1,15 @@
 package com.bcp0109.springwebfluxkotlin.mono
 
+import com.bcp0109.springwebfluxkotlin.coroutine.corouter.CoRouterController
 import com.bcp0109.springwebfluxkotlin.domain.Member
+import org.omg.CORBA.ServerRequest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @Controller
@@ -12,9 +17,11 @@ import reactor.core.publisher.Mono
 class MemberController(
     private val memberService: MemberService
 ) {
+    val log: Logger = LoggerFactory.getLogger(MemberController::class.java)
 
     @GetMapping("/home")
     fun home(): Mono<ResponseEntity<String>> {
+        log.info("home request")
         return Mono.just(
             ResponseEntity.ok().body("Home")
         )
@@ -25,6 +32,20 @@ class MemberController(
         val template = RestTemplate()
         val response = template.getForObject("https://www.google.com", String::class.java)
         return Mono.just(response!!)
+    }
+
+    @GetMapping("/api/mine")
+    fun google2(): Mono<ResponseEntity<String>> {
+        log.info("mine request")
+        val webClient = WebClient.builder().build()
+
+        return webClient.get()
+            .uri("http://localhost:8181/mine")
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .map {
+                ResponseEntity.ok().body(it)
+            }
     }
 
     @GetMapping("/signup")
