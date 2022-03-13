81e3138cdea7b6acb3fb956d3e2a3c77f62c3340
ParkJiwoon <bcp0109@gmail.com>
2022-03-13 21:59:02 +0900

    spring-webflux-kotlin: 쓰레드 확인을 위한 API 요청
    - /mine: webClient 요청 시 어떤 쓰레드에서 요청하는지 확인하기 위해 필터 추가
    - /home: 무거운 CPU 연산을 수행하는 API

--- spring-webflux-kotlin/src/main/kotlin/com/bcp0109/springwebfluxkotlin/mono/router/RouterController.kt
+++ spring-webflux-kotlin/src/main/kotlin/com/bcp0109/springwebfluxkotlin/mono/router/RouterController.kt
@@ -2,9 +2,13 @@ package com.bcp0109.springwebfluxkotlin.mono.router
 import com.bcp0109.springwebfluxkotlin.mono.MemberService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.server.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Controller
@@ -14,8 +18,17 @@ class RouterController(
     val log: Logger = LoggerFactory.getLogger(RouterController::class.java)
 
     fun home(request: ServerRequest): Mono<ServerResponse> {
+        val time = System.currentTimeMillis()
+
         return ServerResponse.ok().json().body(
-            Mono.just("home")
+            Mono.fromCallable {
+                (0..1_000_000_000).forEach {
+                    if (it % 1000 == 0) {
+                        log.info("Request [$time] for: $it")
+                    }
+                }
+                "home"
+            }
         )
     }
 
@@ -26,6 +39,23 @@ class RouterController(
         return ServerResponse.ok().json().body(
             Mono.just(response!!)
         )
+    }
+
+    fun mine(request: ServerRequest): Mono<ServerResponse> {
+        log.info("mine request")
+        val webClient = WebClient.builder()
+            .filters { it.add(logRequest()) }
+            .build()
+
+        return webClient.get()
+            .uri("http://localhost:8181/mine")
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .flatMap {
+                ServerResponse.ok().json().body(
+                    Mono.just(it)
+                )
+            }
     }
 
     fun signup(request: ServerRequest): Mono<ServerResponse> {
@@ -50,5 +80,12 @@ class RouterController(
     fun findAll(request: ServerRequest): Mono<ServerResponse> {
         val members = memberService.findAll()
         return ServerResponse.ok().json().body(members)
+    }
+
+    fun logRequest(): ExchangeFilterFunction {
+        return ExchangeFilterFunction.ofRequestProcessor { clientRequest ->
+            log.info("webclient request")
+            Mono.just(clientRequest)
+        }
     }
 }
