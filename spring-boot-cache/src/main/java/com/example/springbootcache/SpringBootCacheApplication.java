e0d7fcbfc4b6538c3d5956896486303e83ba205d
ParkJiwoon <bcp0109@gmail.com>
2022-03-21 01:48:18 +0900

    spring-boot-cache: first commit

---
+++ spring-boot-cache/src/main/java/com/example/springbootcache/SpringBootCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCacheApplication.class, args);
+    }
+
+}
---
+++ spring-boot-cache/src/test/java/com/example/springbootcache/SpringBootCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
