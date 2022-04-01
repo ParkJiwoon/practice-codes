8df74cf044ecc22a961119393f0e69d0da9ab456
ParkJiwoon <bcp0109@gmail.com>
2022-04-02 02:40:11 +0900

    spring-security-jwt: RefreshToken Entity 에 Column 추가

--- spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/entity/RefreshToken.java
+++ spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/entity/RefreshToken.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -15,17 +16,20 @@ import javax.persistence.Table;
 public class RefreshToken {
 
     @Id
+    @Column(name = "rt_key")
     private String key;
-    private String value;
 
-    public RefreshToken updateValue(String token) {
-        this.value = token;
-        return this;
-    }
+    @Column(name = "rt_value")
+    private String value;
 
     @Builder
     public RefreshToken(String key, String value) {
         this.key = key;
         this.value = value;
+    }
+
+    public RefreshToken updateValue(String token) {
+        this.value = token;
+        return this;
     }
 }
