cdd3f4b93ed42ff22e7030bd74b5a3ec72fbab2c
ParkJiwoon <bcp0109@gmail.com>
2022-05-07 23:43:21 +0900

    Kotlin Koans: Builders Task 1, 2

--- Kotlin Koans/Builders/String and map builders/src/Task.kt
+++ Kotlin Koans/Builders/String and map builders/src/Task.kt
@@ -1,6 +1,11 @@
 import java.util.HashMap
 
 /* TODO */
+fun buildMutableMap(build: MutableMap<Int, String>.() -> Unit): Map<Int, String> {
+    val map = mutableMapOf<Int, String>()
+    map.build()
+    return map
+}
 
 fun usage(): Map<Int, String> {
     return buildMutableMap {
