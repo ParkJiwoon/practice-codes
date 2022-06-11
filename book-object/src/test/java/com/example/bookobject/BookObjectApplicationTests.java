45cc16c7fb7a6c73c06d529ea5ef390a2f21e90e
ParkJiwoon <bcp0109@gmail.com>
2022-06-11 22:34:58 +0900

    book-object: first commit

---
+++ book-object/src/test/java/com/example/bookobject/BookObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookObjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
