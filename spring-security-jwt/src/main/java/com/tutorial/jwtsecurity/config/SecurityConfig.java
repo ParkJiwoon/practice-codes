815d4df1f3111b1311838919ab534104694720a8
BaeSangHyuck <tkdgur961024@gmail.com>
2022-11-01 11:53:00 +0900

    indent수정 및 yml포트 삭제

--- spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/config/SecurityConfig.java
+++ spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/config/SecurityConfig.java
@@ -21,54 +21,55 @@ import javax.servlet.FilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
-  private final TokenProvider tokenProvider;
-  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  // h2 database 테스트가 원활하도록 관련 API 들은 전부 무시
-  @Bean
-  public WebSecurityCustomizer webSecurityCustomizer() {
-    return (web) -> web.ignoring()
-      .antMatchers("/h2-console/**", "/favicon.ico");
-  }
+    // h2 database 테스트가 원활하도록 관련 API 들은 전부 무시
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/h2-console/**", "/favicon.ico");
+    }
 
-  @Bean
-  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    // CSRF 설정 Disable
-    http.csrf().disable()
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정 Disable
+        http.csrf().disable()
 
-      // exception handling 할 때 우리가 만든 클래스를 추가
-      .exceptionHandling()
-      .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-      .accessDeniedHandler(jwtAccessDeniedHandler)
+                // exception handling 할 때 우리가 만든 클래스를 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
 
-      // h2-console 을 위한 설정을 추가
-      .and()
-      .headers()
-      .frameOptions()
-      .sameOrigin()
+                // h2-console 을 위한 설정을 추가
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
 
-      // 시큐리티는 기본적으로 세션을 사용
-      // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
-      .and()
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // 시큐리티는 기본적으로 세션을 사용
+                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-      // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
-      .and()
-      .authorizeRequests()
-      .antMatchers("/auth/**").permitAll()
-      .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
+                // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
-      // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
-      .and()
-      .apply(new JwtSecurityConfig(tokenProvider));
-    return http.build();
-  }
+                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+        return http.build();
+    }
 }
