6bc248a7efe6344c54658543b100ad3eacbb1255
ParkJiwoon <bcp0109@gmail.com>
2022-11-22 23:00:04 +0900

    spring-security-jwt: bearer 수정

--- spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/jwt/TokenProvider.java
+++ spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/jwt/TokenProvider.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
 public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
-    private static final String BEARER_TYPE = "bearer";
+    private static final String BEARER_TYPE = "Bearer";
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
