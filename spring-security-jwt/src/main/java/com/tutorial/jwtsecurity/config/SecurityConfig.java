0d1087e893928e63bf7bd888eb6134b41dcd51a1
ParkJiwoon <bcp0109@gmail.com>
2022-11-23 00:34:44 +0900

    spring-security-jwt: @Configuration 으로 변경

--- spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/config/SecurityConfig.java
+++ spring-security-jwt/src/main/java/com/tutorial/jwtsecurity/config/SecurityConfig.java
@@ -5,17 +5,15 @@ import com.tutorial.jwtsecurity.jwt.JwtAuthenticationEntryPoint;
 import com.tutorial.jwtsecurity.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
