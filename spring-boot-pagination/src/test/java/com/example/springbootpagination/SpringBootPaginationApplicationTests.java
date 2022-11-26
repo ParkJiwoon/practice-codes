8e7bcec21f031a6296abb00bc8ae82dace7f5624
ParkJiwoon <bcp0109@gmail.com>
2022-11-27 04:36:50 +0900

    spring-boot-pagination: first commit

---
+++ spring-boot-pagination/src/test/java/com/example/springbootpagination/SpringBootPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaginationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
