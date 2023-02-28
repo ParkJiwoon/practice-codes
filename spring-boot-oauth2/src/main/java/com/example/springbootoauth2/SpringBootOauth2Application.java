b4e80aa8f848b3ac740bf4dd91620085c5c06083
ParkJiwoon <bcp0109@gmail.com>
2023-02-28 21:47:30 +0900

    spring-boot-oauth2: first commit

---
+++ spring-boot-oauth2/src/main/java/com/example/springbootoauth2/SpringBootOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2Application.class, args);
+    }
+
+}
---
+++ spring-boot-oauth2/src/test/java/com/example/springbootoauth2/SpringBootOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
