8f3f9067525eb539365497a59ddc7e76c9777fe3
ParkJiwoon <bcp0109@gmail.com>
2023-03-03 23:01:54 +0900

    spring-boot-oauth2: config 파일 위치 변경

--- spring-boot-oauth2/src/main/java/com/example/springbootoauth2/config/ClientConfig.java
+++ spring-boot-oauth2/src/main/java/com/example/springbootoauth2/config/ClientConfig.java
@@ -1,4 +1,4 @@
-package com.example.springbootoauth2.oauth.config;
+package com.example.springbootoauth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
