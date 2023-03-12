d17f5414d272a7ce4d016e5db82605292198efb9
ParkJiwoon <bcp0109@gmail.com>
2023-03-12 21:20:31 +0900

    spring-boot-oauth2: Access Token 검증 API 추가

--- spring-boot-oauth2/src/main/java/com/example/springbootoauth2/member/application/MemberController.java
+++ spring-boot-oauth2/src/main/java/com/example/springbootoauth2/member/application/MemberController.java
@@ -1,10 +1,12 @@
 package com.example.springbootoauth2.member.application;
 
+import com.example.springbootoauth2.authentication.domain.AuthTokensGenerator;
 import com.example.springbootoauth2.member.domain.Member;
 import com.example.springbootoauth2.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +17,16 @@ import java.util.List;
 @RequestMapping("/api/members")
 public class MemberController {
     private final MemberRepository memberRepository;
+    private final AuthTokensGenerator authTokensGenerator;
 
     @GetMapping
     public ResponseEntity<List<Member>> findAll() {
         return ResponseEntity.ok(memberRepository.findAll());
+    }
+
+    @GetMapping("/{accessToken}")
+    public ResponseEntity<Member> findByAccessToken(@PathVariable String accessToken) {
+        Long memberId = authTokensGenerator.extractMemberId(accessToken);
+        return ResponseEntity.ok(memberRepository.findById(memberId).get());
     }
 }
