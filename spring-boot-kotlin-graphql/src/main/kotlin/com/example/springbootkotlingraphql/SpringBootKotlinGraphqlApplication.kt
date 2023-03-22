70745349f80d23fef000f04e3bd90cf1415bdc70
ParkJiwoon <bcp0109@gmail.com>
2023-03-23 00:32:03 +0900

    spring-boot-kotlin-graphql: first commit

---
+++ spring-boot-kotlin-graphql/src/main/kotlin/com/example/springbootkotlingraphql/SpringBootKotlinGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootkotlingraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinGraphqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinGraphqlApplication>(*args)
+}
